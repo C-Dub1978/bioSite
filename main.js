c14003ea78e63b6f95cb84e1d2da9e6640f1a2ae
Chris Wilson <chris@myubercode.com>
2018-10-19 21:53:50 -0600

    updated js file

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 let links = document.querySelectorAll('a');
+
+/**
+ * Since there is a fixed navbar, when clicking a normal anchor link
+ * the page scrolls the anchor position to the top of the viewport but the fixed
+ * navbar hides it. This will offset the scroll position after clicking a link
+ * so that the actual anchor point is offset by 200 px
+ */
 links.forEach((link) => {
   link.addEventListener('click', () => {
     setTimeout(() => {
